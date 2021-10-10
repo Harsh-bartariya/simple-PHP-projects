735c0cc8fa8ddf940b67e686459854d3707a30c2
Sunny Kewat <82700347+sukehiro-San@users.noreply.github.com>
2021-10-10 21:17:28 +0530

    Update index.php
    update code for database connection.

--- login/index.php
+++ login/index.php
@@ -22,7 +22,28 @@
 		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
     </div>
   </form>
-  <!-->add php code for validate user credentials from database here<-->
+  <?php
+	if (isset($_POST['login']))
+		{
+			$username = mysqli_real_escape_string($con, $_POST['user']);
+			$password = mysqli_real_escape_string($con, $_POST['pass']);
+			
+			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+			$row		= mysqli_fetch_array($query);
+			$num_row 	= mysqli_num_rows($query);
+			
+			if ($num_row > 0) 
+				{			
+					$_SESSION['user_id']=$row['user_id'];
+					header('location:home.php');
+					
+				}
+			else
+				{
+					echo 'Invalid Username and Password Combination';
+				}
+		}
+  ?>
   <div class="reminder">
     <p>Not a member? <a href="signup.html">Sign up now</a></p>
     <p><a href="#">Forgot password?</a></p>

bc70162a8660062b86ac37f0f073503e67a884e3
Ashutoshonly <55246922+Ashutoshonly@users.noreply.github.com>
2021-10-13 13:14:58 +0530

    solution added

--- login/home.php
+++ login/home.php
@@ -12,11 +12,51 @@ $row=mysqli_fetch_array($result);
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
-<!-->add style for button here<-->
+<style>
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 28px;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+</style>
 <body>
 <div class="form-wrapper"> 
     <center><h3>Welcome: <?php echo $row['username']; ?> </h3></center>
-	<!-->add button here for booking ticket<-->
+	<button class="button" style="vertical-align:middle" href="/booking/booking.html"><span><a href="booking/booking.html">Book ticket here</a></span></button>
 	 <div class="reminder">
     <p><a href="logout.php">Log out</a></p>
   </div>
